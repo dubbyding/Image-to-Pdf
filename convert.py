2e9cc406d71b6106d2cbc43c0fda4db689e36408
dubbyding <rmaharjan.rm90@gmail.com>
2020-09-14 01:36:18 +0545

    Folder to put image for conversion

--- convert.py
+++ convert.py
@@ -6,7 +6,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 initial = 0
 image_list = []
 
-
+# Checking if pdf folder exists and checks to name the file
 if not os.path.isdir(dir_path+'/pdf'):
     os.makedirs(dir_path+'/pdf')
 else:
@@ -15,18 +15,21 @@ else:
         name = '1'
     else:
         name = str(int(name[-1].split('.')[0: -1][0]) + 1)
-for entry in os.scandir(dir_path):
+
+for entry in os.scandir(dir_path+'/Images'):
     if entry.is_dir():
         continue
     ext = entry.name.split('.')[-1]
     if ext == 'py':
         continue
     if initial == 0:
-        im1 = Image.open(dir_path+'/'+entry.name, mode='r').convert('RGB')
+        im1 = Image.open(dir_path+'/Images/'+entry.name, mode='r').convert('RGB')
         initial = initial + 1
-        continue
-    image_list.append(Image.open(dir_path+'/'+entry.name, mode='r').convert('RGB'))
-    if image_list == []:
-        im1.save(dir_path+'/pdf/'+name+'.pdf', mode='r')
     else:
-        im1.save(dir_path+'/pdf/'+name+'.pdf', mode='r',save_all = True, append_images = image_list)
+        image_list.append(Image.open(dir_path+'/Images/'+entry.name, mode='r').convert('RGB'))
+    os.remove(dir_path+'/Images/'+entry.name)
+
+if image_list == []:
+    im1.save(dir_path+'/pdf/'+name+'.pdf', mode='r')
+else:
+    im1.save(dir_path+'/pdf/'+name+'.pdf', mode='r',save_all = True, append_images = image_list)
